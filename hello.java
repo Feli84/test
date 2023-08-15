3badaddc6c2211df7e86d9a6f4e394709e605de5
Feli84 <139757171+Feli84@users.noreply.github.com>
2023-08-15 13:36:20 +0200

    gruessen erstellt update hello

--- hello.java
+++ hello.java
@@ -1,6 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Git");
-		System.out.println("This is a new line.");
-	}
+        System.out.println("This is a new line.");
+
+        Gruessen gruessen = new Gruessen("Dani");
+        System.out.println(gruessen.gruesse());
+    }
 }
