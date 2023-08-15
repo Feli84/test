5a8175d5f2f64cacbc24c7c2e9620ae8ddc74c71
Feli84 <139757171+Feli84@users.noreply.github.com>
2023-08-15 14:08:31 +0200

    änderung in der new line

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Git");
-        System.out.println("This is a new line.");
+        System.out.println("This is a new line in master.");
 
         Gruessen gruessen = new Gruessen("Dani");
         System.out.println(gruessen.gruesse());
